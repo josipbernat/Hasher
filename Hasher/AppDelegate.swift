c2d55bd05c0476d8cdc82ba07e3b114554832c16
Josip Bernat <josip.bernat@clover-studio.com>
2015-07-23 14:02:59 +0200

    Initial Commit

---
+++ Hasher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Hasher
+//
+//  Created by Josip Bernat on 7/23/15.
+//  Copyright (c) 2015 Josip Bernat. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
